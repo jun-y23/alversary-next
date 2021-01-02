6a541da7c6fd8e924fd4efd7fe5c2d45620fe515
jun-y23 <ki8109smith@gmail.com>
2021-01-02 15:46:00 +0900

    excahnge position name and artist

--- pages/_albumList.js
+++ pages/_albumList.js
@@ -4,13 +4,15 @@ const RenderAlbumList = (props) => {
     return (
         <div key={props.year} className={styles.albumList}>
             <p className={styles.albumListYear}>{props.year}</p>
+            {/* アルバム単位で分割するか */}
             <div className={styles.albumListAlbums}>{props.albums.map((album) => (
                 <div key={album._id} className="item">
                     <a href={album.uri} target="_blank">
                         <img src={album.images[0].url} style={{width: "150px"}}></img>
                     </a>
-                    <p>{album.artist}</p>
                     <p>{album.name}</p>
+                    <p>{album.artist}</p>
+        
                 </div>
             ))}</div>
         </div>
