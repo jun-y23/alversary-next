923f2180e5ff484687f7241b213052ed99328c18
jun-y23 <ki8109smith@gmail.com>
2021-01-04 18:04:56 +0900

    amend type error

--- pages/album.tsx
+++ pages/album.tsx
@@ -2,11 +2,26 @@ import styles from "../styles/Index.module.scss";
 import RenderAlbumList from "./_albumList";
 import { ObjectId } from "mongodb";
 
-type Props = {
+interface Props {
 	albumArray: {
 		releasedYear: string,
-		albums: []
+		albums: [AlbumItem]
 	}[];
+}
+
+interface AlbumItem {
+	_id: ObjectId,
+	name: string,
+	artist: string,
+	release_date: string,
+	uri: string,
+	images: [Image],
+}
+
+interface Image {
+    height: number,
+    url: string,
+    width: number
 }
 
 export default function Album(props: Props) {
@@ -34,15 +49,15 @@ export async function getServerSideProps() {
 	const json = await res.json();
 	const albums: {}[] = json.albums;
 
-	// リリース年ごとに分割
-	// [{'releasedYear':'2000','albums': []}]
+	// リリース年ごとに分割 [{'releasedYear':'2000','albums': []},]
 	let albumArray: {
 		releasedYear: string,
-		albums: []
+		// @TODO: anyやめる
+		albums:any []
 	}[] = [];
 	
+	// @TODO: anyやめる
 	albums.map((album: any) => {
-		// ここでObject.Assingして新しいやつ作るか。
 		let releasedYear: string = album.release_date.substr(0, 4);
 	
 		if (!albumArray.find((val) => val.releasedYear === releasedYear)) {
@@ -55,7 +70,7 @@ export async function getServerSideProps() {
 		const targetObj = albumArray.find((val) => val.releasedYear === releasedYear);
 		targetObj?.albums.push(album)
 	});
-	return {
+	return { 
 		props: {
 			albumArray,
 		},
