253aeea48a4f5c65d5b416ec28c8dc42e8ce5280
jun-y23 <ki8109smith@gmail.com>
2021-01-07 23:30:53 +0900

    make sam endpoint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,48 +3,50 @@ import RenderAlbumList from "../components/_albumList";
 import { ObjectId } from "mongodb";
 
 interface Props {
-	albumArray: {
-		releasedYear: string,
-		albums: [AlbumItem]
-	}[];
+    albumArray: {
+        releasedYear: string;
+        albums: [AlbumItem];
+    }[];
 }
 
 interface AlbumItem {
-	_id: ObjectId,
-	name: string,
-	artist: string,
-	release_date: string,
-	uri: string,
-	images: [Image],
+    _id: ObjectId;
+    name: string;
+    artist: string;
+    release_date: string;
+    uri: string;
+    images: [Image];
 }
 
 interface Image {
-    height: number,
-    url: string,
-    width: number
+    height: number;
+    url: string;
+    width: number;
 }
 
 export default function Home(props: Props) {
-	if (props.albumArray.length) {
-		return (
-			<main className={styles.main}>
-				<h1 className={styles.title}>Alversary</h1>
-				<div className={styles.albumsArea}>
-					<ul className={styles.yearList}>
-						{props.albumArray.map((item, index) => {
-							return <RenderAlbumList key={index} year={item.releasedYear} albums={item.albums} />;
-						})}
-					</ul>
-				</div>
-			</main>
-		);
-	}
-	return (
-		<main>
-			no albums released...
-		</main>
-	)
-};
+    if (props.albumArray.length) {
+        return (
+            <main className={styles.main}>
+                <h1 className={styles.title}>Alversary</h1>
+                <div className={styles.albumsArea}>
+                    <ul className={styles.yearList}>
+                        {props.albumArray.map((item, index) => {
+                            return (
+                                <RenderAlbumList
+                                    key={index}
+                                    year={item.releasedYear}
+                                    albums={item.albums}
+                                />
+                            );
+                        })}
+                    </ul>
+                </div>
+            </main>
+        );
+    }
+    return <main>no albums released...</main>;
+}
 
 /**
  *
@@ -52,38 +54,39 @@ export default function Home(props: Props) {
  * @return array
  */
 export async function getServerSideProps() {
-	const endpoint: string = process.env.API_ENDPOINT as string
-	const res = await fetch(endpoint);
-	const json = await res.json();
-	const albums: {}[] = json.albums;
+	const endpoint: string = process.env.API_ENDPOINT as string;
+    const res = await fetch(endpoint);
+	const albums: {}[] = await res.json();
 
-	// リリース年ごとに分割 [{'releasedYear':'2000','albums': []},]
-	let albumArray: {
-		releasedYear: string,
-		// @TODO: anyやめる
-		albums:any []
-	}[] = [];
+    // リリース年ごとに分割 [{'releasedYear':'2000','albums': []},]
+    let albumArray: {
+        releasedYear: string;
+        // @TODO: anyやめる
+        albums: any[];
+    }[] = [];
 
-	if (albums.length) {
-		// @TODO: anyやめる
-		albums.forEach((album: any) => {
-			let releasedYear: string = album.release_date.substr(0, 4);
+    if (albums.length) {
+        // @TODO: anyやめる
+        albums.forEach((album: any) => {
+            let releasedYear: string = album.release_date.substr(0, 4);
 
-			if (!albumArray.find((val) => val.releasedYear === releasedYear)) {
-				albumArray.push({
-					releasedYear: releasedYear,
-					albums: []
-				});
-			} 
+            if (!albumArray.find((val) => val.releasedYear === releasedYear)) {
+                albumArray.push({
+                    releasedYear: releasedYear,
+                    albums: [],
+                });
+            }
 
-			const targetObj = albumArray.find((val) => val.releasedYear === releasedYear);
-			targetObj?.albums.push(album)
-		});
-	}
+            const targetObj = albumArray.find(
+                (val) => val.releasedYear === releasedYear
+            );
+            targetObj?.albums.push(album);
+        });
+    }
 
-	return { 
-		props: {
-			albumArray,
-		},
-	};
-};
+    return {
+        props: {
+            albumArray,
+        },
+    };
+}
