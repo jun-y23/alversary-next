dfc0e51bdbbf83bb4ad4972d4733e3c54abc6c8d
jun-y23 <ki8109smith@gmail.com>
2022-03-16 16:24:28 +0100

    add unit test for index

---
+++ tests/index.test.tsx
@@ -0,0 +1,56 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import { Home, Props } from '../src/pages/index';
+import { ObjectId } from "mongodb";
+
+describe('Rendering', () => {
+	let dummyProps: Props;
+	beforeEach(() => {
+		dummyProps = {
+			itemList: [
+				{
+				title: 'Albums',
+				description: 'dummy description',
+				classification:'year',
+				type: 'album',
+				itemList: [
+					{
+						heading: '2000',
+						itemList: [
+							{
+								_id: new ObjectId(1),
+								name: '',
+								artist: '',
+								release_date: '',
+								uri: '',
+								images: [
+									{
+										height: 100,
+										width: 100,
+										url: '',
+									},
+									{
+										height: 100,
+										width: 100,
+										url: '',
+									},
+									{
+										height: 100,
+										width: 100,
+										url: '',
+									},
+								]
+							}
+						],
+					}
+				],
+			}
+		]};
+	});
+
+	it('It should render', () => {
+		render(<Home {...dummyProps} />)
+		expect(screen.getByText(dummyProps.itemList[0].title)).toBeInTheDocument();
+	})
+})
